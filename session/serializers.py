ce4fdc0baba9a89cdb45e47bf9dca635d3b65594
Michael Marner <michael@20papercups.net>
2019-03-15 05:50:50 +1030

    Return user's groups

--- session/serializers.py
+++ session/serializers.py
@@ -4,9 +4,10 @@ from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
 
+    groups = serializers.StringRelatedField(many = True)
     class Meta:
         model = User
-        fields = ('id', 'username','first_name','last_name','email')
+        fields = ('id', 'username','first_name','last_name','email','groups','is_staff','is_superuser','is_active')
         write_only_fields = ('password',)
-        read_only_fields = ('is_staff','is_superuser','is_active','date_joined',)
+        read_only_fields = ('is_staff','is_superuser','is_active','date_joined','groups',)
 
