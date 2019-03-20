94928d5bae809bec63f90e3870d871da27c3629d
Michael Marner <michael@20papercups.net>
2019-03-20 22:42:29 +1030

    Optionally serve downloads through Django

--- downloads/views.py
+++ downloads/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import get_object_or_404, Http404
 from .models import DownloadLink
 from django.http import HttpResponse
-
+from django.views.static import serve;
+from django.conf import settings
+import os
 
 def download(request, linkID):
   """ Returns a file through Apache's X-Sendfile header, if the link is valid"""
@@ -11,10 +13,13 @@ def download(request, linkID):
     raise Http404("Invalid download link: " + linkID)
 
   if link.isCurrent():
-    response = HttpResponse()
-    response['X-Sendfile'] = link.path
-    response['X-Accel-Redirect'] = link.path + ';'
-    return response
+    if settings.DOWNLOAD_X_SENDFILE:
+      return serve(request, os.path.basename(link.path), os.path.dirname(link.path))
+    else:
+      response = HttpResponse()
+      response['X-Sendfile'] = link.path
+      response['X-Accel-Redirect'] = link.path + ';'
+      return response
   else:
     raise Http404("Download link expired: " + linkID)
 
