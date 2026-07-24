6231a2b9693c01b26e1aed01d351ab82108b977c
Ben Thatcher <149745752+btgs-0@users.noreply.github.com>
2026-07-24 20:07:34 +0930

    Sneak in drf-spectacular. Comment out the URLS so no one can just open them up.

--- logger/settings/base.py
+++ logger/settings/base.py
@@ -65,12 +65,21 @@ WSGI_APPLICATION = "logger.wsgi.application"
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "PAGE_SIZE": 100,
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Three D Radio API",
+    "DESCRIPTION": "Three D Radio's API",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    # OTHER SETTINGS
 }
 
 # Static files (CSS, JavaScript, Images)
--- logger/settings/installed_apps.py
+++ logger/settings/installed_apps.py
@@ -8,6 +8,7 @@ INSTALLED_APPS = (
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_filters",
+    "drf_spectacular",
     "rest_framework",
     "rest_framework.authtoken",
     "corsheaders",
