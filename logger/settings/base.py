575bd51aeab4bee9160c54181e171000ba58f5b4
Ben Thatcher <149745752+btgs-0@users.noreply.github.com>
2026-07-28 18:52:19 +0930

    Update API listed version to 1.0.2

--- logger/settings/base.py
+++ logger/settings/base.py
@@ -77,7 +77,7 @@ REST_FRAMEWORK = {
 SPECTACULAR_SETTINGS = {
     "TITLE": "Three D Radio API",
     "DESCRIPTION": "Three D Radio's API",
-    "VERSION": "1.0.0",
+    "VERSION": "1.0.2",
     "SERVE_INCLUDE_SCHEMA": False,
     # OTHER SETTINGS
 }
