b06b99d3d9e9ce4e4519a7dad5af4aadc6c837e3
Ben Thatcher <149745752+btgs-0@users.noreply.github.com>
2026-09-09 07:52:12 +0930

    Add sentry initialization to settings. Read environment variables for DSN.

--- logger/settings.py
+++ logger/settings.py
@@ -1,4 +1,9 @@
+import os
+
 from split_settings.tools import include, optional
+import logging
+import sentry_sdk
+from sentry_sdk.integrations.logging import LoggingIntegration
 
 DATABASES = {
     "default": {
@@ -20,4 +25,20 @@ include(
     "settings/downloads.py",
     optional("local_settings.py"),
     scope=globals(),
+)
+
+
+# Set up Sentry logging levels
+sentry_logging = LoggingIntegration(
+    level=logging.INFO,        # Captures INFO and above as breadcrumbs
+    event_level=logging.ERROR  # Captures ERROR and above as Sentry events
+)
+
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN"),
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    # Don't keep anything.
+    send_default_pii=False,
+    integrations=[sentry_logging],
 )
